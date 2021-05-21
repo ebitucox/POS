fedd997f7abc0898b5f54e45f4429402472888c1
ebit_817 <ebit.ucox@gmail.com>
2021-05-21 15:15:18 +0800

    konfigurasi login controller dan model

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,7 +10,17 @@ class Auth extends CI_Controller
 
     public function proses()
     {
+        $post = $this->input->post(null, TRUE);
 
-        echo "proses";
+        if (isset($post['login'])) {
+
+            $this->load->model('user_m');
+            $query = $this->user_m->login($post);
+            if ($query->num_rows() > 0) {
+                echo "login berhasil";
+            } else {
+                echo 'login gagal';
+            }
+        }
     }
 }
