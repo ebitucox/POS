138460f7758ffe6a28c936342282249bba225271
ebit_817 <ebit.ucox@gmail.com>
2021-05-24 11:43:01 +0800

    selesai membuat tampilan user form add

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -14,6 +14,7 @@ class User extends CI_Controller
     }
     public function add()
     {
+
         $this->template->load('template', 'user/user_form_add');
     }
 }
--- application/views/User/user_form_add.php
+++ application/views/User/user_form_add.php
@@ -1,9 +1,9 @@
 <!-- Content Wrapper. Contains page content -->
 
 <!-- Content Header (Page header) -->
-<section class="content-header">
+<section class="content-header ml-3">
     <h1>DATA
-        <small>user</small>
+        <small><strong>user</small>
     </h1>
     <ol class="breadcrumb">
         <li><a href=""><i class="fa fa-dashbord"></i></a></li>
@@ -14,16 +14,67 @@
 
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title"><b>DATA USER</b></h3>
-                <div style="float: right !important;">
-                    <a href="<?= base_url('user/add') ?>" class="btn btn-primary btn-flat mb-2">
-                        <i class="fa fa-user-plus"></i>
-                        create
-                    </a>
+
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2>Add User</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <a href="<?= base_url('user') ?>" class="btn btn-info btn-flat mb-2" style="float: right !important;">
+                            <i class="fa fa-undo"></i>
+                            Back
+                        </a>
+                    </div>
+
+
                 </div>
             </div>
-            <div class="box-body">
+            <div class="box-body ml-3">
+                <div class="row">
+                    <div class="col-md-4">
+                        <form action="" class="mt-3" method="POST">
+                            <div class="form-group">
+                                <label for="">Name *</label>
+                                <input type="text" name="fullname" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Username</label>
+                                <input type="text" name="username" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Password</label>
+                                <input type="password" name="password" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Password Confirmation </label>
+                                <input type="password" name="passconf" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Address</label>
+                                <textarea name="address" class="form-control">
+                                </textarea>
+                                <!-- <input type="password" name="password" class="form-control"> -->
+                            </div>
+                            <div class="form-group">
+                                <label for="">Level</label>
+                                <select name="level" class="form-control">
+                                    <option value="">Pilih</option>
+                                    <option value="1">Admin</option>
+                                    <option value="2">User</option>
 
+                                </select>
+
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-flat">
+                                    <i class="fa fa-paper-plane"></i>
+                                    Save</button>
+                                <button type="reset" class="btn btn-secondary btn-flat">Reset</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
 
 
