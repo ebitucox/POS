c594aa663a7037ca75029f8894f5fcb5aec1ed84
ebit_817 <ebit.ucox@gmail.com>
2021-05-25 17:46:23 +0800

    membuat model supplier

--- application/controllers/Supplier.php
+++ application/controllers/Supplier.php
@@ -5,6 +5,6 @@ class Supplier extends CI_Controller
 {
     public function index()
     {
-        $this->template->load('template', 'supplier/supplier_data');
+        $this->template->load('template ', 'supplier/supplier_data');
     }
 }
