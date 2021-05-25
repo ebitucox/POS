5f269c971b0371d3b0805351c85fc258b2f7e97e
ebit_817 <ebit.ucox@gmail.com>
2021-05-25 18:19:57 +0800

    membuat fitur supplier

--- application/controllers/Supplier.php
+++ application/controllers/Supplier.php
@@ -3,8 +3,18 @@
 
 class Supplier extends CI_Controller
 {
+
+    function __construct()
+    {
+        parent::__construct();
+        check_not_login();
+        // check_admin();
+        $this->load->model('supplier_m');
+    }
+
     public function index()
     {
+        $data['row'] = $this->supplier_m->get();
         $this->template->load('template ', 'supplier/supplier_data');
     }
 }
---
+++ application/models/Supplier_m.php
@@ -0,0 +1,20 @@
+<?php
+
+class Supplier_m extends CI_Model
+{
+
+
+    public function get($id = null)
+    {
+
+        $this->db->select('*');
+        $this->db->from('supplier');
+        if ($id != null) { // menampilkan  data berdasarkan parameter id jika ada data yang dikirimkan
+
+            $this->db->where('supplier_id', $id);
+        }
+        // menampilkan data keselurah pada tabel karna tidakn ada data pada parameter id yang dikirim
+        $query = $this->db->get();
+        return $query;
+    }
+}
